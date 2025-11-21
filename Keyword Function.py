e9c8abd3cafdc9abe46ab1d9920d2d466892232c
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-11-21 23:51:11 +0530

    Add files via upload

---
+++ Keyword Function.py
@@ -0,0 +1,24 @@
+# keyword function:
+
+
+def details(name, father, mother, age, phone, address, course):
+    print(f'''Hello {name}, my Dad's name is {father}, my mother's name is {mother}, my age is {age}, my phone no is {phone}, my address is {address}, my course is {course}.''')
+
+details(
+    name='M.Loka Srinivas',
+    father='Sankara Narayana',
+    mother='Nageswaramma',
+    age=26,
+    phone=8309957022,
+    address='Vijayawada',   # Add your address here
+    course='MCA'
+)
+
+
+
+
+
+
+
+
+
