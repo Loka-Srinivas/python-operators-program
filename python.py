9e340eb6cb8e4f51555fa15489957bfd13dfb541
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-11-22 00:30:03 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,29 @@
+print("hii iam srinivas")
+print("long the value")
+print("all the value of the image the system")
+
+
+
+name1="loka srinivas"
+name2="mani kanth"
+temp=""
+temp=name1
+name1=name2
+name2=temp
+print(name1)
+print(name2)
+x=1.5
+y=6
+z=9
+z=x
+y=x
+x=y
+print(x,y)
+a="l"
+b="o"
+c="k"
+d="a"
+print("\n"+a+"\n"+b+"\n"+c+"\n"+d+"\n")
+print("\n"+d+"\n"+c+"\n"+b+"\n"+a+"\n")
+
+print(6>>2)
