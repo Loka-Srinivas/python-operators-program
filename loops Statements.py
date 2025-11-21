b861855d5f1323693910203540339ce4aeb65fef
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-11-22 00:23:41 +0530

    Add files via upload

---
+++ loops Statements.py
@@ -0,0 +1,72 @@
+# list1=[1,2,3,4,5]
+
+# for i in list1:
+#     print("Hello", i)
+
+
+
+
+# list1=[1,2,3,4,5,6,7,8,9,10]
+
+# for i in list1:
+#     print("Hello World", i)
+
+
+
+
+# list1=list(range(1,51))
+# print(list1)    
+
+# for i in list1:
+#      print("Hello World", i)
+
+
+
+# for i in range(1,11,5):
+#     print(i)
+
+
+
+# list1=list(range(1,51))
+# print(len(list1))
+
+
+# list1=["apple", "banana", "orange", "grapes", "mangos"]
+# print(len(list1))
+
+
+# for i in range(len(list1)):
+#     print(i, list1[1])
+
+
+
+
+#for loop on strings
+# string1="Loka Srinivas"
+# for i in string1:
+#     print(i)
+
+
+# string1="Loka Srinivas"
+# for i in range(len(string1)):
+#     print(i, string1[i])
+
+  
+# nested for loop
+
+# for i in range(8):
+#     for j in range(5):
+        # print(i,j,  end='')
+
+# for i in range(8):
+#     for j in range(5):
+#         print('i value',i, 'j value',j)
+
+
+# list1=['Mancha']
+# list2=['Sankara Narayana', 'Loka Srinivas', 'Supraja', 'Sri Lakshmi']
+# for i in list1:
+#     for j in list2:
+#         print(i,j)
+
+
